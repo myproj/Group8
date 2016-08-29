601f84fd11a4b06c67035aa60bd4b00de5a9d4fa
vadlurivivek <vadlurivivek@gmail.com>
2016-08-29 21:47:56 +0530

    modified for -ve numbers

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include "functions.h"
 using namespace std;
 int main(){
+    int n;
     print_hello();
     cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
-    return 0;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    char choice='y';
+    while(choice == 'y'){
+      if(n>0){
+	cout<<"factorial="<<factorial(n)<<endl;
+	cout<<"press n to exit \n y to renter:";
+	cin>>choice;
+      }else{
+	cout<<"renter +ve value";
+	cin>>n;
+      }
+    }
 }
