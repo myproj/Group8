53072996bf8acf7a07801c7f85eb13a5fd219dcd
vadlurivivek <vadlurivivek@gmail.com>
2016-09-05 16:30:13 +0530

    added user interface

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+#include <stdio.h>
 #include "functions.h"
 using namespace std;
 int main(){
     int n;
     print_hello();
     cout<< endl;
-    cout<<"enter a number"<<endl;
-    cin>>n;
+    
     char choice='y';
     while(choice == 'y'){
+      cout<<"enter a number:";//changed bugs here
+      cin>>n;
       if(n>0){
 	cout<<"factorial="<<factorial(n)<<endl;
+	printf("factorial= %d\n",factorial(n));
 	cout<<"press n to exit \n y to renter:";
 	cin>>choice;
       }else{
